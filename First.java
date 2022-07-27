ef989835fe564d8e579b35ced3d72ce3b9a2592b
Satyam <mesatyamsharma@gmail.com>
2022-07-27 20:57:35 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,7 @@
+class First{
+    public static void main ( String[]arg) {
+        System.out.println("hello");
+        System.out.println("my first day on java");
+        System.out.println("I am working on Hp victus");
+    }
+}
